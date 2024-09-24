1b42d927aaac9d1354200d106f7d83a327192605
Bernice-Nkirote <bernicemwirigi@gmail.com>
2024-09-24 11:37:58 +0300

    first commit

---
+++ Array Methods/script.js
@@ -0,0 +1,43 @@
+// LENGTH
+// const fruits = ["Banana","Orange","Apple","Mango"];
+// let size = fruits.length;
+// console.log(size);
+
+// const fruits =["Banana","Orange","Apple","Mango"];
+// let listOfFruits = fruits.toString();
+// console.log(listOfFruits);
+
+//JOIN()
+// const fruits =["Banana","Orange","Apple","Mango"];
+// let text = fruits.join("-");
+// console.log(text);
+
+//POP
+// const fruits =["Banana","Orange","Apple","Mango"];
+// let newtext = fruits.pop();
+// console.log(newtext);
+// console.log(fruits);
+
+// PUSH
+// const fruits = ["Banana","Orange","Apple","Mango"];
+// let morefruits = fruits.push("Avacado");
+// console.log(morefruits);
+// console.log(fruits);
+
+// Shift method
+// const fruits = ["Banana","Orange","Apple","Mango"];
+// let text = fruits.shift();
+// console.log(fruits);
+
+// Unshift method
+// const fruits = ["Banana","Orange","Apple","Mango"];
+// let newfruits = fruits.unshift("Lemon");
+// console.log(newfruits);
+// console.log(fruits);
+
+// CONCAT()
+const femaleEmployees = ["Cecile", "Eunice"];
+const maleEmployees = ["John", "Mathew", "Mark"];
+
+let employees = femaleEmployees.concat(maleEmployees);
+console.log(employees);
---
+++ String Methods/index.js
@@ -0,0 +1,43 @@
+// let alphabet = "ABCDEFGHIJKLMNOP";
+// console.log(alphabet.length);
+
+// Charat menthod
+// let text = "HELLO WORLD";
+// let char = text.charAt(0);
+// console.log(char);
+
+// at method
+// const name = "PowerLearnProject";
+// let letter = name.at(-2);
+// console.log(letter);
+
+//SLICE METHOD(START, END)
+
+// let fruits = "Apple, Banana, Orange";
+// let fruit = fruits.slice(7);
+// console.log(fruit);
+// let part = fruits.slice(7,13);
+// console.log(part);
+
+// SUBSTRING METHOD
+// let countries = "Madagascar, Kenya, Nigeria";
+// let part = countries.substring(7,13);
+// console.log(part);
+
+// LOWERCASE AND UPPERCASE METHODS
+// UPPERCASE
+// let greeting = "Hello there!";
+// let text2 = greeting.toLowerCase();
+// console.log(text2);
+// let text = greeting.toUpperCase();
+// console.log(text);
+
+// CONCAT
+let text = "Hello" + " " +"World"; 
+let texthello = "Hello".concat(" ", "World!");
+
+console.log(texthello);
+
+
+
+
